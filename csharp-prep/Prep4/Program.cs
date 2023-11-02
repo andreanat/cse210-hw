f35d4ccbeb2b9f186f0cae7692f30fca9c604e72
andreanat <andrea.nmb13@gmail.com>
2023-11-02 16:53:38 -0600

    list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+           List<int> numbers = new List<int>();
+        int input;
+
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+                numbers.Add(input);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        } while (input != 0);
+
+        Console.WriteLine("Numbers entered:");
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
+    
