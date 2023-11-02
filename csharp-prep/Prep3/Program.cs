3ba7f22b889ecd99d5db5875eb8942e6160163bb
andreanat <andrea.nmb13@gmail.com>
2023-11-02 16:21:29 -0600

    aa

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -14,6 +14,7 @@ class Program
         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
 
             if (guess < magicNumber)
             {
