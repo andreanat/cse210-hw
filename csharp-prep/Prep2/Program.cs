cad150bab383fc0cd841454f82fd1e84b0b78ff3
andreanat <andrea.nmb13@gmail.com>
2023-11-02 15:32:34 -0600

    mess

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -14,6 +14,15 @@ class Program
         {
             Console.WriteLine("Invalid input. Please enter a valid numeric score.");
         }
+
+         if (Message(grade))
+            {
+                Console.WriteLine("Congratulations! Keep up the good work.");
+            }
+            else
+            {
+                Console.WriteLine("Keep working hard. You can do better next time!");
+            }
     }
 
     static string Grade(double score)
@@ -39,4 +48,16 @@ class Program
             return "F";
         }
     }
+      static string Message(string grade)
+    {
+        string message;
+        if (grade != "F")
+        {
+            message = "Congratulations! You passed the course.";
+        }
+        else
+        {
+            message = "Keep working hard. You can do better next time!";
+        }
+        return message;
 }
