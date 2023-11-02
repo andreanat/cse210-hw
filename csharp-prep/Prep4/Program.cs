f77e9572f84fbe6541247308d5eee7138b81d493
andreanat <andrea.nmb13@gmail.com>
2023-11-02 17:08:10 -0600

    finishded

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -26,6 +26,25 @@ class Program
         {
             Console.WriteLine(number);
         }
-    }
+//sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+//average
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+//max
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
 }
-    
+}
