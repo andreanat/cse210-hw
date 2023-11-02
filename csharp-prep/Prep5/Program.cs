522a098e29cddec4719f52cac9cef918e9559fba
andreanat <andrea.nmb13@gmail.com>
2023-11-02 17:21:18 -0600

    next

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -19,15 +19,21 @@ class Program
         Console.WriteLine("Welcome to the Program!");
     }
     static string PromptUserName(){
-
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
     }
     static int PromptUserNumber(){
-
+        do
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        } 
     }
     static int SquareNumber(){
 
     }
     static void DisplayResult(){
-        
+
     }
     }
