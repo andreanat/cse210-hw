27f69b5d6dc7c5d6e0c0726829b1da265504dc4c
andreanat <andrea.nmb13@gmail.com>
2023-11-02 16:13:13 -0600

    first attempt

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        Console.WriteLine("I've selected a random number between 1 and 100. Try to guess it!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Try guessing higher.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Try guessing lower.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number: " + magicNumber);
+            }
+        }
+
     }
 }
