d8624b1fb50d1e26016aa7d46f1ebb093b6a07bb
andreanat <andrea.nmb13@gmail.com>
2023-11-02 15:29:56 -0600

    base

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Pleas introduce your grade for the course: ");
+        if (double.TryParse(Console.ReadLine(), out double score))
+        {
+            string grade = Grade(score);
+            Console.WriteLine($"Your letter grade for the course is: {grade}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid numeric score.");
+        }
+    }
+
+    static string Grade(double score)
+    {
+        if (score >= 90)
+        {
+            return "A";
+        }
+        else if (score >= 80)
+        {
+            return "B";
+        }
+        else if (score >= 70)
+        {
+            return "C";
+        }
+        else if (score >= 60)
+        {
+            return "D";
+        }
+        else
+        {
+            return "F";
+        }
     }
 }
