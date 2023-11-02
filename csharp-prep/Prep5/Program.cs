bef0f1afc9aafaf90ab82091c41bb518c2bebd5a
andreanat <andrea.nmb13@gmail.com>
2023-11-02 17:17:31 -0600

    start

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
     }
-}
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName(){
+
+    }
+    static int PromptUserNumber(){
+
+    }
+    static int SquareNumber(){
+
+    }
+    static void DisplayResult(){
+        
+    }
+    }
