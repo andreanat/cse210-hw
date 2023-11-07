150aefb26cc0fe0984479e8a48c4abb32248118f
andreanat <andrea.nmb13@gmail.com>
2023-11-07 16:59:48 -0700

    get

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+class Fraction{
+    private int _top;
+    private int _bottom;
+
+    //constructor1
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+
+        //constructor2
+    public Fraction(int _top1){
+        _top = _top1;
+        _bottom = 1;
+    }
+
+    //constructor3
+       public Fraction(int _top1, int _bottom1){
+        _top = _top1;
+        _bottom = _bottom1;
+    }
+    public int GetNumerator(){
+        return _top;
     }
 }
