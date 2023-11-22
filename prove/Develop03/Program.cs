881e8a0f57ef9ef7019d86c3b4a784b9e9fc1ff7
andreanat <andrea.nmb13@gmail.com>
2023-11-21 19:33:02 -0700

    class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
     }
+class Scripture{
+
+}
+class Reference{
+    
+}
+class Word{
+
+}
 }
