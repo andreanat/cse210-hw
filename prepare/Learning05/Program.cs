3c77c59c9f5db136b92a90f85694b519c17837c7
andreanat <andrea.nmb13@gmail.com>
2023-11-30 19:18:10 -0700

    test TV

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,19 +1,26 @@
 using System;
-using System.Collections.Generic;
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         List<Shape> shapes = new List<Shape>();
 
-        shapes.Add(new Square("Red", 5.0));
-        shapes.Add(new Rectangle("Blue", 4.0, 6.0));
-        shapes.Add(new Circle("Green", 3.0));
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
 
-        foreach (Shape shape in shapes)
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
         {
-            Console.WriteLine($"Color: {shape.Color}, Area:{shape.GetArea()}"); 
-        }
 
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
