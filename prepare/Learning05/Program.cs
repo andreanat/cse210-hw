1a501fd2070ceebe5e805e20f0a69d67d8268085
andreanat <andrea.nmb13@gmail.com>
2023-11-30 19:00:32 -0700

    circle

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -9,6 +9,12 @@ class Program
         Console.WriteLine($"Color: {square.Color}");
         Console.WriteLine($"Area: {square.GetArea()}");
 
+        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
+        Console.WriteLine($"Rectangle - Color: {rectangle.Color}, Area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 3.0);
+        Console.WriteLine($"Circle - Color: {circle.Color}, Area: {circle.GetArea()}");
+
 
     }
 }
