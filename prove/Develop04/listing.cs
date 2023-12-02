1e5d1be8365cf5e52b91c7ab6370d328b4147bb5
andreanat <andrea.nmb13@gmail.com>
2023-12-01 17:40:25 -0700

    correctef

--- prove/Develop04/listing.cs
+++ prove/Develop04/listing.cs
@@ -15,9 +15,11 @@ class ListingActivity : Activity
 
     private void InitializePrompts()
     {
-        _prompts.Add("Prompt 1");
-        _prompts.Add("Prompt 2");
-        // Add more prompts
+        _prompts.Add("Who are people that you appreciate?");
+        _prompts.Add("What are personal strengths of yours?");
+        _prompts.Add("Who are people that you have helped this week?");
+        _prompts.Add("When have you felt the Holy Ghost this month?");
+        _prompts.Add("Who are some of your personal heroes?");
     }
 
     public override void Run()
